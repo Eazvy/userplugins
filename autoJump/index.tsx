4caace691e84144e39de7a134ad7e2159953fab5
Eazvy <57739965+Eazvy@users.noreply.github.com>
2026-02-03 08:21:07 -0500

    add autoJump

---
+++ autoJump/index.tsx
@@ -0,0 +1,40 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+
+import { EquicordDevs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { Menu, ComponentDispatch } from "@webpack/common";
+import { NavContextMenuPatchCallback } from "@api/ContextMenu";
+
+let lastChannelId: string | null = null;
+const MenuPatch: NavContextMenuPatchCallback = (children) => {
+    children.push(
+        <Menu.MenuItem
+            id="auto-jump"
+            label="Jump to Last Message"
+            action={() => ComponentDispatch.dispatch("SCROLLTO_PRESENT")}
+        />
+    );
+};
+
+export default definePlugin({
+    name: "AutoJump",
+    description: "Automatically jump to the bottom when switching channels.",
+    authors: [EquicordDevs.omaw],
+    contextMenus: {
+        "channel-context": MenuPatch,
+        "user-context": MenuPatch,
+        "thread-context": MenuPatch
+    },
+    flux: {
+        CHANNEL_SELECT({ channelId, messageId }) {
+            if (!channelId || messageId || lastChannelId === channelId) return;
+            lastChannelId = channelId;
+            ComponentDispatch.dispatch("SCROLLTO_PRESENT");
+        }
+    }
+});
